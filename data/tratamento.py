c5634454f0a8c53b9cfa03aea11e401abedb7f22
leoAboard <leonardoarmelin2003@outlook.com>
2025-11-28 06:11:58 -0400

    feat: querys

--- data/tratamento.py
+++ data/tratamento.py
@@ -1,41 +1,60 @@
 import pandas as pd
 from flask import Flask, request, jsonify
-from sqlalchemy import create_engine, text
+from sqlalchemy import text
 import os
-from dotenv import load_dotenv
 import psycopg2
 from datetime import datetime
+from database import engine
 
 SECRET = os.environ.get('SERVICE_TOKEN')
 
-load_dotenv()
-
-BD_USER = os.getenv("BD_USER")
-BD_PASS = os.getenv("BD_PASS")
-HOST = os.getenv("HOST")
-BD_PORT = os.getenv("BD_PORT")
-BD_NAME = os.getenv("BD_NAME")
-
 app = Flask(__name__)
-engine = create_engine(
-    f"postgresql+psycopg2://{BD_USER}:{BD_PASS}@{HOST}:{BD_PORT}/{BD_NAME}"
-)
-
 @app.route("/data", methods=["POST"])
-# def handle_data():
-#     return processar_graficos()
 
-def processar_graficos():
+def triagem():
     auth = request.headers.get('Authorization', '')
     if(auth != SECRET):
         return jsonify({"error": "Unauthorized"}), 401
     
     data = request.get_json()
-    date_str = data.get('ano_limite')
+    data_limite = data.get('ano_limite')
+    contagem = data.get('contagem')
+    redes_sociais = data.get('redes_sociais')
+
+    params = {}
+    if(data_limite != '-1'):
+        data_limite_sup = datetime(int(data_limite), 1, 1).date()
+        data_limite_inf = datetime(int(data_limite), 12, 31).date()
+        params['data_limite_sup'] = data_limite_sup
+        params['data_limite_inf'] = data_limite_inf
+
+    if(contagem):
+        return contagem_alunos(params)
+    if(redes_sociais):
+        return contagem_rede_social(params)
+
+    return "erro"
+
+def contagem_alunos(params):
+    query = "SELECT COUNT(*) FROM aluno"
+
+    if(params):
+        query += " WHERE data_nascimento BETWEEN :data_limite_sup AND :data_limite_inf"
 
     with engine.connect() as connection:
-        query = text("SELECT * FROM aluno WHERE id > 2")
-        result = connection.execute(query)
+        result = connection.execute(text(query), params)
+        df = pd.DataFrame(result.fetchall(), columns=result.keys())
+
+    return(df.to_json(orient='records'))
+
+def contagem_rede_social(params):
+    query = "SELECT rede_social FROM preferencia"
+
+    if(params):
+        query += " AND data_nascimento BETWEEN :data_limite_sup AND :data_limite_inf"
+
+    with engine.connect() as connection:
+        result = connection.execute(text(query), params)
         df = pd.DataFrame(result.fetchall(), columns=result.keys())
 
     return(df.to_json(orient='records'))
